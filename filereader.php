f0c179ce547ee5273f12c18f6590b3df93f8d419
f varas <fvaras@smartjog.com>
2012-12-04 17:05:38 +0100

    still not working

--- filereader.php
+++ filereader.php
@@ -1,32 +1,40 @@
 <?php
 $current_map=$_GET["elev"];
 if (isset($current_map))
-{
-echo "current map present";
-}
-else
-{
-$current_map=$argv[1];
-}
-if(!$current_map)
-{
-die("no map provided\n");
-}
-else
-{
-echo "map provided from the command line\n";
-}
-$data=fopen($current_map,'r');
+  {
+  echo "current map present";
+  }
+  else
+  {
+  $current_map=$argv[1];
+  }
 
-//$time0=preg_match('/<time>/',$data);
-echo "time0: $time0";
+if(!$current_map)
+  {
+  die("no map provided\n");
+  }
+  else
+  {
+  echo "map provided from the command line\n";
+  }
+$data=fopen($current_map,'r');
+$first_bool = True;
+$first = "";
+
 while (!feof($data)) {
   $line = fgets($data);
   if(preg_match('/<ele>/',$line))
     $elevation=trim(strip_tags($line));
   if(preg_match('/<time>/',$line))
     $timestamp=strtotime(trim(strip_tags($line),"\t"));
-  echo "$elevation"." $timestamp\n";
+    echo "[$line]";
+    if($first_bool) 
+      {
+      $first = $timestamp;
+      $first_bool = False;
+      echo "first: $first";
+      }
+  echo "$elevation "."$first "."$timestamp\n";
 }
 fclose($data);
 ?>
